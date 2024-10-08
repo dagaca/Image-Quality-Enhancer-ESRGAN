ed1d7f3197a1347736409b2866f3419e5bec30a1
Damla <80363244+dagaca@users.noreply.github.com>
2024-10-08 15:51:34 +0300

    Update run.py

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ from app import app  # Import the 'app' variable from the app module
 
 if __name__ == '__main__':
     # Run the Flask application in debug mode and make it accessible externally
-    app.run(debug=True, host="0.0.0.0")
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
